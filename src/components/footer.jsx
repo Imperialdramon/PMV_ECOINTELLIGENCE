4e18853d9a302f1731d11ac30441b2784b9fdc20
Imperialdramon <pabloelperez14@gmail.com>
2023-10-22 18:31:37 -0300

    Footer tuneado

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,6 +5,8 @@ import { IconButton, Stack, Typography } from "@mui/material"
 import { Instagram, LinkedIn } from "@mui/icons-material"
 import Divider from "@mui/material/Divider"
 import Logo_2 from '../assets/Logo_2.png'
+import FmdGoodIcon from '@mui/icons-material/FmdGood'
+import EmailIcon from '@mui/icons-material/Email'
 
 function Footer() {
     return (
@@ -21,7 +23,7 @@ function Footer() {
             boxShadow={3}
             >
             <Grid container spacing={2}>
-                <Grid xs>
+                <Grid xs={4}>
                     <Box
                         justifyContent="center"
                         display="flex"
@@ -31,26 +33,27 @@ function Footer() {
                         <img src={Logo_2} alt="logo2" width={"80px"}></img>
                     </Box>
                 </Grid>
-                <Grid xs={4} container direction="column" justifyContent="flex-start" alignItems="flex-start">
-                    <Typography variant="h5">Contacto</Typography>
-                    <Stack>
-                        <Typography>Coordenadas</Typography>
-                        <Typography>Santiago, Chile</Typography>
-                        <Typography>321 calle ecológica</Typography>
+                <Grid xs={5} container direction="column" justifyContent="flex-start">
+                    <div style={{ textAlign: 'center' }}>
+                        <Typography variant="h5" align='center' sx={{fontWeight:'bold'}} marginRight={"25%"}>Contacto</Typography>
+                    </div>
+                    <Stack textAlign={"justify"} flexWrap={"wrap"}>
+                        <Typography> <FmdGoodIcon fontSize="body2" color="#5ba3e5"/> Av. Vicuña Mackenna 3939, 8940897 San Joaquín, Región Metropolitana</Typography>
+                        <Typography> <EmailIcon fontSize="body2" color="#5ba3e5"/> ecointelligence.neds@gmail.com</Typography>
                     </Stack>
                 </Grid>
-                <Grid xs={2} container direction="column" justifyContent="center">
-                    <Typography variant="h5">Compartir</Typography>
-                    <Stack direction="row" alignItems="center" divider={<Divider orientation="vertical" flexItem />}>
-                        <IconButton><svg xmlns="http://www.w3.org/2000/svg" height="2rem" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" fill="#FFFFFF"/></svg></IconButton>
-                        <IconButton><Instagram fontSize="large" sx={{color:"primary.icon"}}/></IconButton>
-                        <IconButton><LinkedIn fontSize="large" sx={{color:"primary.icon"}}/></IconButton>
+                <Grid xs={3} container direction="column" justifyContent="center" alignItems={"center"}>
+                    <Typography variant="h5" align='center' sx={{fontWeight:'bold'}}>Compartir</Typography>
+                    <Stack direction="row" divider={<Divider orientation="vertical" flexItem />}>
+                        <IconButton item><svg xmlns="http://www.w3.org/2000/svg" height="2rem" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" fill="#FFFFFF"/></svg></IconButton>
+                        <IconButton item><Instagram fontSize="large" sx={{color:"primary.icon"}}/></IconButton>
+                        <IconButton item><LinkedIn fontSize="large" sx={{color:"primary.icon"}}/></IconButton>
                     </Stack>
                 </Grid>
             </Grid>
             <Grid  container direction="column" justifyContent="center" alignItems="center">
                 <Typography variant="p" className="col-sm">
-                    &copy;{new Date().getFullYear()} EcoIntelligence | All rights reserved | Terms Of Service | Privacy
+                    &copy;{new Date().getFullYear()} EcoIntelligence | Todos los derechos reservados.
                 </Typography>
             </Grid>
         </Box>
